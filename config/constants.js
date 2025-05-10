95acefe3e71d361a284e81b3369f8b75f5d4c5f6
nvuknfsjoln <shopmyvideos24.7@gmail.com>
2025-05-10 13:21:26 +0100

    Create constants.js

---
+++ config/constants.js
@@ -0,0 +1,12 @@
+module.exports = {
+  ROLES: {
+    ADMIN: 'admin',
+    CREATOR: 'creator',
+    USER: 'user',
+  },
+  SUPPORT_EMAIL: 'support@shopmyvideos.com',
+  JWT_EXPIRES_IN: '7d',
+  MAX_VIDEO_SIZE_MB: 500,
+  VIDEO_UPLOAD_DIR: 'uploads/videos',
+  PREVIEW_IMAGE_DIR: 'uploads/previews',
+};
