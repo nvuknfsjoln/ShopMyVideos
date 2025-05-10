c43a17a833d53fd9694b47d0c27badefbac17842
nvuknfsjoln <shopmyvideos24.7@gmail.com>
2025-05-10 12:19:42 +0100

    Delete backend directory

--- backend/middleware/creatorAuth.js
+++
@@ -1,8 +0,0 @@
-function creatorOnly(req, res, next) {
-    if (!req.user || req.user.role !== 'creator') {
-        return res.status(403).json({ error: 'Creator access required' });
-    }
-    next();
-}
-
-module.exports = creatorOnly;
