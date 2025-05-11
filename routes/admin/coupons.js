5bac2e38cd65fde3b8af9f192c2e9e5917b6cb85
nvuknfsjoln <shopmyvideos24.7@gmail.com>
2025-05-11 10:43:23 +0100

    Create coupons.js

---
+++ routes/admin/coupons.js
@@ -0,0 +1,19 @@
+// routes/admin/coupons.js
+const express = require('express');
+const router = express.Router();
+const { requireAdminAuth } = require('../../middleware/auth');
+const { listAllCoupons, deleteCoupon } = require('../../controllers/couponController');
+
+router.use(requireAdminAuth);
+
+router.get('/', async (req, res) => {
+  const coupons = await listAllCoupons();
+  res.render('admin/coupons', { coupons });
+});
+
+router.post('/delete/:id', async (req, res) => {
+  await deleteCoupon(req.params.id);
+  res.redirect('/admin/coupons');
+});
+
+module.exports = router;
