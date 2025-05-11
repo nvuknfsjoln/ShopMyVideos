362623bbb8f6ad02298d5d0fdc2ae3914af0e38b
nvuknfsjoln <shopmyvideos24.7@gmail.com>
2025-05-11 08:20:52 +0100

    Update creatorController.js

--- controllers/creatorController.js
+++ controllers/creatorController.js
@@ -2,6 +2,37 @@ const Creator = require('../models/Creator');
 const Video = require('../models/Video');
 const Voucher = require('../models/Voucher'); // Wichtig: Modell importieren
 
+exports.requestVoucher = async (req, res) => {
+  const { creatorName, code, discount } = req.body;
+  try {
+    const existing = await Voucher.findOne({ code });
+    if (existing) return res.status(400).json({ message: 'Gutscheincode existiert bereits' });
+
+    const voucher = new Voucher({
+      code,
+      discount,
+      isCreatorVoucher: true,
+      creatorName,
+      requested: true,
+      valid: false
+    });
+    await voucher.save();
+    res.status(201).json({ message: 'Gutscheinanfrage eingereicht' });
+  } catch (error) {
+    res.status(500).json({ message: 'Fehler bei Gutscheinanfrage' });
+  }
+};
+
+exports.setAdminVoucherPreference = async (req, res) => {
+  const { creatorName, allowAdminVouchers } = req.body;
+  try {
+    await Creator.findOneAndUpdate({ creatorName }, { allowAdminVouchers });
+    res.status(200).json({ message: 'Einstellung aktualisiert' });
+  } catch (error) {
+    res.status(500).json({ message: 'Fehler beim Aktualisieren der Einstellung' });
+  }
+};
+
 // Creator anmelden
 exports.registerCreator = async (req, res) => {
   const { creatorName, creatorPassword, email } = req.body;
