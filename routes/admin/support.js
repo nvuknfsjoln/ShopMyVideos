e066f33cab69501eba5e2720006fa5538ea21756
nvuknfsjoln <shopmyvideos24.7@gmail.com>
2025-05-11 10:43:42 +0100

    Create support.js

---
+++ routes/admin/support.js
@@ -0,0 +1,19 @@
+// routes/admin/support.js
+const express = require('express');
+const router = express.Router();
+const { requireAdminAuth } = require('../../middleware/auth');
+const { getAllTickets, respondToTicket } = require('../../controllers/supportController');
+
+router.use(requireAdminAuth);
+
+router.get('/', async (req, res) => {
+  const tickets = await getAllTickets();
+  res.render('admin/support', { tickets });
+});
+
+router.post('/respond/:id', async (req, res) => {
+  await respondToTicket(req.params.id, req.body.response);
+  res.redirect('/admin/support');
+});
+
+module.exports = router;
