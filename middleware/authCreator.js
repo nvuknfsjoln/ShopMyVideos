092f6026f76f4b4a0209820eb93c97ac397a6c23
nvuknfsjoln <shopmyvideos24.7@gmail.com>
2025-05-11 11:52:58 +0100

    Update authCreator.js

--- middleware/authCreator.js
+++ middleware/authCreator.js
@@ -1,7 +1,11 @@
-// middleware/authCreator.js
-module.exports = (req, res, next) => {
+// Middleware, die sicherstellt, dass der Creator eingeloggt ist
+module.exports.requireCreatorAuth = (req, res, next) => {
+  // Überprüfen, ob der Creator in der Session vorhanden ist
   if (req.session && req.session.creatorId) {
+    // Wenn der Creator eingeloggt ist, fahre mit der Anfrage fort
     return next();
+  } else {
+    // Falls nicht eingeloggt, Weiterleitung zur Login-Seite
+    return res.redirect('/creator/login');
   }
-  res.redirect('/creator/login');
 };
