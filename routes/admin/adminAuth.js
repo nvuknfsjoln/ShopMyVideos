d14ee56bbe90a061c3b4ab0fc5c206fef8fd323d
nvuknfsjoln <shopmyvideos24.7@gmail.com>
2025-05-11 06:43:37 +0100

    Update adminAuth.js

--- routes/admin/adminAuth.js
+++ routes/admin/adminAuth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const adminAuthController = require('../../controllers/admin/adminAuthController');
+const adminAuthController = require('../../controllers/adminController');
 
 // Admin einloggen
 router.post('/login', adminAuthController.loginAdmin);
