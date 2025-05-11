041c726ddf0b25ae17da3ca7db49a43866675219
nvuknfsjoln <shopmyvideos24.7@gmail.com>
2025-05-11 12:33:13 +0100

    Update authAdmin.js

--- middleware/authAdmin.js
+++ middleware/authAdmin.js
@@ -1,7 +1,10 @@
 // middleware/authAdmin.js
-module.exports = (req, res, next) => {
+
+const requireAdminAuth = (req, res, next) => {
   if (req.session && req.session.adminId) {
     return next();
   }
   res.redirect('/admin/login');
 };
+
+module.exports = { requireAdminAuth };
