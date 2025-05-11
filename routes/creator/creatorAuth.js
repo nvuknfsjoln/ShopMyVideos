08e7c281658d6c555ddeb2b8cbc69defe94ce2cc
nvuknfsjoln <shopmyvideos24.7@gmail.com>
2025-05-11 06:57:32 +0100

    Update creatorAuth.js

--- routes/creator/creatorAuth.js
+++ routes/creator/creatorAuth.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const creatorAuthController = require('../../controllers/creator/creatorAuthController');
+const creatorController = require('../../controllers/creatorController');
 
 // Creator registrieren (mit Ü18-Check)
-router.post('/register', creatorAuthController.registerCreator);
+router.post('/register', creatorController.registerCreator);
 
 // Creator einloggen
-router.post('/login', creatorAuthController.loginCreator);
+router.post('/login', creatorController.loginCreator);
 
 // Gutschein beantragen
-router.post('/request-coupon', creatorAuthController.requestCoupon);
+router.post('/request-voucher', creatorController.requestVoucher);
 
 module.exports = router;
