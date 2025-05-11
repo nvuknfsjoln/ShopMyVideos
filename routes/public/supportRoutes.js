d8336007193258e67caed6386171ad71e710d28f
nvuknfsjoln <shopmyvideos24.7@gmail.com>
2025-05-11 07:00:56 +0100

    Update supportRoutes.js

--- routes/public/supportRoutes.js
+++ routes/public/supportRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const supportController = require('../../controllers/public/supportController');
+const supportController = require('../../controllers/supportController');
 
 // Supportnachricht absenden
 router.post('/send', supportController.sendMessage);
