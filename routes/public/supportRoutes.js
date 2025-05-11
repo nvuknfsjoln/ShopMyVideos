013522aaeb8f81939da410975c14e04e6d405345
nvuknfsjoln <shopmyvideos24.7@gmail.com>
2025-05-11 08:54:26 +0100

    Delete routes directory

--- routes/public/supportRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const supportController = require('../../controllers/supportController');
-
-// Supportnachricht absenden
-router.post('/send', supportController.sendSupportMessage);
-
-module.exports = router;
