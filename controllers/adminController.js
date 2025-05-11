27b156e8c725afcbe95e55d22003c23ad9b56a65
nvuknfsjoln <shopmyvideos24.7@gmail.com>
2025-05-11 08:06:13 +0100

    Update adminController.js

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,52 +1,23 @@
-const Admin = require('../models/Admin');
-const Video = require('../models/Video');
-const Creator = require('../models/Creator');
-const Coupon = require('../models/Voucher');
+const Voucher = require('../models/Voucher');
 
-// Admin-Login
-exports.loginAdmin = async (req, res) => {
-  const { adminName, adminPassword } = req.body;
+// Alle Gutscheine abrufen
+exports.getAllVouchers = async (req, res) => {
   try {
-    const admin = await Admin.findOne({ adminName, adminPassword });
-    if (!admin) {
-      return res.status(400).json({ message: 'Ungültige Anmeldedaten' });
-    }
-    res.status(200).json({ message: 'Erfolgreich eingeloggt' });
+    const regularVouchers = await Voucher.find({ isCreatorVoucher: false });
+    const creatorVouchers = await Voucher.find({ isCreatorVoucher: true });
+    res.status(200).json({ regularVouchers, creatorVouchers });
   } catch (error) {
-    res.status(500).json({ message: 'Fehler beim Login' });
+    res.status(500).json({ message: 'Fehler beim Laden der Gutscheine' });
   }
 };
 
-// Admin erstellen
-exports.createAdmin = async (req, res) => {
-  const { adminName, adminPassword } = req.body;
+// Gutschein freigeben
+exports.approveCreatorVoucher = async (req, res) => {
+  const { code } = req.body;
   try {
-    const newAdmin = new Admin({ adminName, adminPassword });
-    await newAdmin.save();
-    res.status(201).json({ message: 'Admin erfolgreich erstellt' });
+    await Voucher.findOneAndUpdate({ code }, { valid: true, requested: false });
+    res.status(200).json({ message: 'Gutschein genehmigt' });
   } catch (error) {
-    res.status(500).json({ message: 'Fehler beim Erstellen des Admins' });
-  }
-};
-
-// Alle Admins anzeigen
-exports.getAdmins = async (req, res) => {
-  try {
-    const admins = await Admin.find();
-    res.status(200).json(admins);
-  } catch (error) {
-    res.status(500).json({ message: 'Fehler beim Laden der Admins' });
-  }
-};
-
-// Video bearbeiten
-exports.updateVideo = async (req, res) => {
-  const { videoId } = req.params;
-  const { title, price, categories } = req.body;
-  try {
-    const video = await Video.findByIdAndUpdate(videoId, { title, price, categories }, { new: true });
-    res.status(200).json(video);
-  } catch (error) {
-    res.status(500).json({ message: 'Fehler beim Bearbeiten des Videos' });
+    res.status(500).json({ message: 'Fehler bei Genehmigung' });
   }
 };
