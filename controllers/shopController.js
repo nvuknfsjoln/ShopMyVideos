44ddd0916ba7f90175b6c60e56fdb94bdd867d1e
nvuknfsjoln <shopmyvideos24.7@gmail.com>
2025-05-11 11:35:14 +0100

    Update shopController.js

--- controllers/shopController.js
+++ controllers/shopController.js
@@ -1,18 +1,35 @@
 // controllers/shopController.js
 
-exports.getShopHome = (req, res) => {
-  res.send('Welcome to the Shop!');
+// Dummy-Daten – später durch Datenbank ersetzen
+const videos = [
+  { id: '1', title: 'Funny Cats Compilation', price: 9.99 },
+  { id: '2', title: 'DIY Home Projects', price: 12.49 },
+  { id: '3', title: 'Top 10 Travel Destinations', price: 14.99 },
+];
+
+// Startseite
+exports.getHome = (req, res) => {
+  res.render('shop/home', {
+    title: 'ShopMyVideos – Deine Videoplattform',
+  });
 };
 
-exports.getProduct = (req, res) => {
-  const productId = req.params.id;
-  res.send(`Details for product with ID: ${productId}`);
+// Alle Videos anzeigen
+exports.getAllVideos = (req, res) => {
+  res.render('shop/videos', {
+    title: 'Alle Videos',
+    videos,
+  });
 };
 
-exports.getCart = (req, res) => {
-  res.send('Your shopping cart is empty.');
-};
-
-exports.checkout = (req, res) => {
-  res.send('Checkout page.');
+// Einzelnes Video anzeigen
+exports.getVideoById = (req, res) => {
+  const video = videos.find(v => v.id === req.params.id);
+  if (!video) {
+    return res.status(404).send('Video nicht gefunden');
+  }
+  res.render('shop/video-detail', {
+    title: video.title,
+    video,
+  });
 };
