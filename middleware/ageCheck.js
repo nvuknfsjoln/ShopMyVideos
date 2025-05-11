81fb775125d6dc53cfae5830993229732c2c09d0
nvuknfsjoln <shopmyvideos24.7@gmail.com>
2025-05-11 17:38:41 +0100

    Update ageCheck.js

--- middleware/ageCheck.js
+++ middleware/ageCheck.js
@@ -1,22 +1,8 @@
 module.exports = function (req, res, next) {
-  // Wenn das Alter bereits in der Session gespeichert ist, weitermachen
   if (req.session && req.session.isAdult) {
-    return next();
+    return next(); // Zugriff erlaubt
   }
 
-  // Prüfen, ob das Alter im POST-Body mitgesendet wurde
-  const age = parseInt(req.body.age, 10);
-
-  if (!isNaN(age)) {
-    if (age >= 18) {
-      // In Session speichern, dass der Nutzer volljährig ist
-      req.session.isAdult = true;
-      return next();
-    } else {
-      return res.status(403).send('Zugriff verweigert: Mindestalter 18 Jahre.');
-    }
-  }
-
-  // Wenn kein Alter übergeben wurde und nichts in der Session ist: Weiterleitung zur Altersabfrage
+  // Keine gültige Session → zur Altersabfrage weiterleiten
   res.redirect('/age-check');
 };
