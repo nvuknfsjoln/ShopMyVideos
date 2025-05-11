cab6ee196b222b8775446fc530fc0efd76dfa741
nvuknfsjoln <shopmyvideos24.7@gmail.com>
2025-05-11 12:42:39 +0100

    Update creators.js

--- routes/admin/creators.js
+++ routes/admin/creators.js
@@ -1,7 +1,7 @@
 // routes/admin/creators.js
 const express = require('express');
 const router = express.Router();
-const { requireAdminAuth } = require('../../middleware/auth');
+const { requireAdminAuth } = require('../../middleware/authAdmin');
 const { listCreators, deleteCreator } = require('../../controllers/creatorController');
 
 router.use(requireAdminAuth);
