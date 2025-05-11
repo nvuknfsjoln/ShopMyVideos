3a8472fd584ef4821f2f58fcaa8c3b0030ccfa5b
nvuknfsjoln <shopmyvideos24.7@gmail.com>
2025-05-11 08:03:58 +0100

    Update Creator.js

--- models/Creator.js
+++ models/Creator.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
 
-// Creator Schema
 const creatorSchema = new mongoose.Schema({
   creatorName: { type: String, required: true, unique: true },
   creatorPassword: { type: String, required: true },
-  email: { type: String, required: true },
-  joinDate: { type: Date, default: Date.now },
-  videos: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Video' }],
+  email: { type: String, required: true, unique: true },
+  allowAdminVouchers: { type: Boolean, default: true } // Steuerung durch Creator
 });
 
 module.exports = mongoose.model('Creator', creatorSchema);
