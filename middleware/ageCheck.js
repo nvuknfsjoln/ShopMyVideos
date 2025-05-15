dbb4743e8293fa7d5350769de95fe654958a5f2d
nvuknfsjoln <shopmyvideos24.7@gmail.com>
2025-05-15 02:59:07 +0100

    Delete middleware directory

--- middleware/ageCheck.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (req, res, next) {
-  if (req.session && req.session.isAdult) {
-    return next(); // Zugriff erlaubt
-  }
-
-  // Keine gültige Session → zur Altersabfrage weiterleiten
-  res.redirect('/age-check');
-};
--- middleware/authAdmin.js
+++
@@ -1,8 +0,0 @@
-// middleware/authAdmin.js
-
-module.exports.requireAdminAuth = (req, res, next) => {
-  if (req.session && req.session.user && req.session.user.role === 'admin') {
-    return next();
-  }
-  return res.redirect('/creator/login'); // absichtlich creator-login
-};
